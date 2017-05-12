da951deca658ad19be18e84d36bab6e94b9817c9
derek villars <derekvillars@yahoo.com>
2017-05-12 14:39:44 -0700

    stuff

---
+++ src/Aboutme/Controllers/GetGithubController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aboutme.Models;
+
+namespace Aboutme.Controllers
+{
+    public class GetGithubController : Controller
+    {
+        public IActionResult Index()
+        {
+            var result = Github.GetGithub();
+            return View(result);
+        }
+    }
+}
---
+++ src/Aboutme/Models/Github.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aboutme.Models
+{
+    [Table("Githubs")]
+    public class Github
+    {
+        public int GithubId { get; set; }
+        public string Name { get; set; }
+        public string Html_Url { get; set; }
+
+
+        public static List<Github> GetGithub()
+        {
+            var client = new RestClient("https://api.github.com/users");
+            var request = new RestRequest("dvillars/starred", Method.GET);
+            request.AddHeader("Accept", "application/vnd.github.v3+json");
+            request.AddHeader("User-Agent", "dvillars");
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            var result = JsonConvert.DeserializeObject<JArray>(response.Content);
+
+            string jsonOutput = result.ToString();
+
+            var githublList = JsonConvert.DeserializeObject<List<Github>>(jsonOutput);
+
+            return githublList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
